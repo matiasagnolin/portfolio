2acc191a3387bb0aca6f079ec9a0ffeec2ffe5fc
matiasa gnolin <matias.agnolin@ifco.com>
2026-02-17 16:05:51 +0100

    feature: adding docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   // base '/' works with custom domain (matiasdev.info)
   // if you ever remove the custom domain, change to '/portfolio/'
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        docs: resolve(__dirname, 'docs.html'),
+      },
+    },
+  },
 })
